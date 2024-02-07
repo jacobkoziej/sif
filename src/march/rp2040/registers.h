0f0c181b03ac03b8a1cb1d86aa4a40b194b153a3
Jacob Koziej <jacobkoziej@gmail.com>
2024-02-07 03:29:39 -0500

    Add `registers.h`

---
+++ src/march/rp2040/registers.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * registers.h -- rp2040 registers
+ * Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef SIF_MARCH_RP2040_REGISTERS_H
+#define SIF_MARCH_RP2040_REGISTERS_H
+
+#endif	// SIF_MARCH_RP2040_REGISTERS_H
