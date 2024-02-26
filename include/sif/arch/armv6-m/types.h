9325fc966bc9394759601074f259d48d56e532e3
Jacob Koziej <jacobkoziej@gmail.com>
2024-02-26 00:24:59 -0500

    Define `sif_arch_armv6_m_exception_handler_t`

---
+++ include/sif/arch/armv6-m/types.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * types.h -- armv6-m types
+ * Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef SIF_ARCH_ARMV6_M_TYPES_H
+#define SIF_ARCH_ARMV6_M_TYPES_H
+
+typedef void sif_arch_armv6_m_exception_handler_t(void);
+
+#endif	// SIF_ARCH_ARMV6_M_TYPES_H
