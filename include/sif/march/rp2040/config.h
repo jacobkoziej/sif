a484d39fc78e14754b7451eb9c2baf8f8dcd7f77
Jacob Koziej <jacobkoziej@gmail.com>
2024-02-26 03:24:26 -0500

    Add `config.h`

---
+++ include/sif/march/rp2040/config.h
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * config.h -- rp2040 default config
+ * Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef SIF_MARCH_RP2040_PORT_CONFIG_H
+#define SIF_MARCH_RP2040_PORT_CONFIG_H
+
+#define SIF_CONFIG_SYSTEM_CLOCK_HZ 125000000ul
+
+#define SIF_CONFIG_SYSTICK_HZ 100
+
+#endif	// SIF_MARCH_RP2040_PORT_CONFIG_H
