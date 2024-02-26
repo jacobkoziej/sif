7c65a71fb5c8898dc68fb9ef4a1bd2d9cbc267c8
Jacob Koziej <jacobkoziej@gmail.com>
2024-02-26 00:05:16 -0500

    Add `port.h`

---
+++ include/sif/march/rp2040/port.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * port.h -- rp2040 port
+ * Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef SIF_MARCH_RP2040_PORT_H
+#define SIF_MARCH_RP2040_PORT_H
+
+#include <sif/march/rp2040/port-asm.h>
+
+#endif	// SIF_MARCH_RP2040_PORT_H
