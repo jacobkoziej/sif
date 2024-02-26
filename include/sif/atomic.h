70142b43fbdfcba4ae7632d89e7f77a3ef036869
Jacob Koziej <jacobkoziej@gmail.com>
2024-02-26 00:12:08 -0500

    Add `atomic.h`

---
+++ include/sif/atomic.h
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * atmoic.h -- atomics
+ * Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef SIF_ATOMIC_H
+#define SIF_ATOMIC_H
+
+#include <sif/port.h>
+
+typedef sif_port_atomic_t sif_atomic_t;
+
+#endif	// SIF_ATMOIC_H
