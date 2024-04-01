60e9715d71841c10d7b52373cd7cc91fc765f9bb
Jacob Koziej <jacobkoziej@gmail.com>
2024-04-01 19:32:39 -0400

    Define `sif_port_get_coreid()`

--- include/sif/private/sif.h
+++ include/sif/private/sif.h
@@ -7,8 +7,10 @@
 #ifndef SIF_PRIVATE_SIF_H
 #define SIF_PRIVATE_SIF_H
 
+#include <sif/port.h>
 #include <sif/sif.h>
 
+extern sif_port_word_t (* const sif_port_get_coreid)(void);
 extern void (* const sif_port_kernel_lock)(void);
 extern void (* const sif_port_kernel_unlock)(void);
 
--- src/march/rp2040/port.c
+++ src/march/rp2040/port.c
@@ -9,12 +9,8 @@
 #include <sif/march/rp2040/port-asm.h>
 #include <sif/march/rp2040/port.h>
 
-void (* const sif_port_task_scheduler_start)(sif_task_stack_t *stack)
-	= sif_march_rp2040_scheduler_start;
-
-sif_task_stack_t *(* const sif_port_task_init_context)(
-	sif_task_stack_t *stack, sif_task_function_t *func, void *arg)
-	= sif_arch_armv6_m_init_context;
+sif_port_word_t (* const sif_port_get_coreid)(void)
+	= sif_march_rp2040_get_coreid;
 
 void (* const sif_port_kernel_lock)(void) = sif_march_rp2040_kernel_lock;
 
@@ -23,3 +19,10 @@ void (* const sif_port_kernel_unlock)(void) = sif_march_rp2040_kernel_unlock;
 sif_syscall_error_t (* const sif_port_syscall)(
 	sif_syscall_t syscall, void * const arg)
 	= sif_arch_armv6_m_syscall;
+
+sif_task_stack_t *(* const sif_port_task_init_context)(
+	sif_task_stack_t *stack, sif_task_function_t *func, void *arg)
+	= sif_arch_armv6_m_init_context;
+
+void (* const sif_port_task_scheduler_start)(sif_task_stack_t *stack)
+	= sif_march_rp2040_scheduler_start;
