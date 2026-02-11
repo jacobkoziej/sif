b9541cc2260069d59798a2caa78bd3270bc147b1
Jacob Koziej <jacobkoziej@gmail.com>
2026-02-11 09:31:04 -0800

    chore: add `aarch64` and `darwin` systems

--- flake.nix
+++ flake.nix
@@ -13,6 +13,9 @@
 
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
         "x86_64-linux"
       ];
 
