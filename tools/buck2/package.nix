0dcb441c73db6a46981a3d3a15056b47b431e6fc
Jacob Koziej <jacobkoziej@gmail.com>
2026-07-12 10:02:17 -0700

    chore: uprev `buck2` to `2026-07-01`

--- tools/buck2/package.nix
+++ tools/buck2/package.nix
@@ -13,13 +13,13 @@
       inherit (pkgs) makeRustPlatform;
       inherit (pkgs.stdenv) isLinux;
 
-      version = "2026-05-01";
+      version = "2026-07-01";
 
       src = fetchFromGitHub {
         owner = "facebook";
         repo = "buck2";
         tag = version;
-        hash = "sha256-C1BKfAIfyJ51OV60XpQpR2wDkxC6GDlf75LJ7KHITXs=";
+        hash = "sha256-OUry9bJjE9R/9hT/GK4oOu/hVtQJrKABrSkn8osWFfk=";
       };
 
       rust = pkgs.rust-bin.fromRustupToolchainFile "${src}/rust-toolchain";
@@ -63,9 +63,9 @@
           cargoLock = {
             lockFile = ./Cargo.lock;
             outputHashes = {
+              "filedescriptor-0.8.3" = "sha256-V6WvkNZryYofarsyfcmsuvtpNJ/c3O+DmOKNvoYPbmA=";
+              "finl_unicode-1.3.0" = "sha256-38S6XH4hldbkb6NP+s7lXa/NR49PI0w3KYqd+jPHND0=";
               "perf-event-0.4.8" = "sha256-Mvfp41Q9g9Z9xgdzFEdIdH/96YeCxrrSl2Vsm6geGMQ=";
-              "probminhash-0.1.12" = "sha256-8IzGV6QDvyBPavICUB4j/VABBkplGa+sSsIz1OD35ik=";
-              "sorted_vector_map-0.2.1" = "sha256-KM9HFTpP22VD05xOj2IEIoe3ITasHVAKTlZkCZGuhdw=";
             };
           };
 
@@ -81,8 +81,12 @@
 
           cargoTestFlags = [
             "--no-fail-fast"
+            "--workspace"
+            "--exclude=dice-examples"
             "--"
             "--skip=net_io::tests::test_network_collector"
+            "--skip=version_control_revision::tests::test_get_git_revision_matches_head"
+            "--skip=version_control_revision::tests::test_get_git_status_clean"
           ];
 
           meta = with lib; {
